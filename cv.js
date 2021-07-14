464f05e52aa0301df91e8a49d269213ee3edfe58
Justice Anisiji <anisijijustice2001@gmail.com>
2021-07-14 21:03:27 +0100

    my changes

---
+++ cv.js
@@ -0,0 +1,37 @@
+var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("1").style.display = "block";
+ }, 1000);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("2").style.display = "block";
+ }, 1200);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("3").style.display = "block";
+ }, 1400);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("4").style.display = "block";
+ }, 1600);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("5").style.display = "block";
+ }, 1800);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("6").style.display = "block";
+ }, 2000);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("7").style.display = "block";
+ }, 2200);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("8").style.display = "block";
+ }, 2400);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("9").style.display = "block";
+ }, 2700);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("10").style.display = "block";
+ }, 3000);
+ var myTimeout = setTimeout( function loadTxt() {
+    document.getElementById("loadtxt").style.display = "none";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("cv").style.display = "block";
+    document.getElementById("cv").style.opacity = "1";
+ }, 5000);
+ 
